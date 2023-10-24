d9d1ca406096731d5a220163dc18c00338faa130
YarraRohit052 <118298129+YarraRohit052@users.noreply.github.com>
2023-10-24 11:29:34 +0530

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -32,7 +32,7 @@ const Dashboard = () => {
                         Institute: Vishnu Institute of Technology
                     </h3>
                     <h3 className='ng-binding'>
-                        Internship: CYBERSECURITY VIRTUAL INTERNSHIP (MAY - JULY 2023)
+                        Internship: DATA ANALYTICS VIRTUAL INTERNSHIP (MAY - JULY 2023)
                     </h3>
                     <br></br>
                 </div>
